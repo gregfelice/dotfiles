4f5ab8b848c0a94c327f21189724919ded069167
gregf <gregf@dweezil>
2025-09-27 15:10:52 -0400

    eza sort by modified

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,7 @@ end
 alias la 'eza -a --color=always --group-directories-first --icons'
 alias ll 'eza -l --color=always --group-directories-first --icons'
 alias ls 'eza -al --color=always --group-directories-first --icons'
-alias lt 'eza -aT --color=always --group-directories-first --icons'
+alias lt 'eza -l --color=always --group-directories-first --icons -s=modified -r'
 
 fish_add_path $HOME/.bin
 fish_add_path $HOME/.local/bin
