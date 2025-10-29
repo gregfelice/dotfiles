92bda30ce7a7ea93d659c75dbf0d05d8439c2c50
Greg Felice <gregfelice@gmail.com>
2025-10-28 22:31:21 -0400

    added tmux.conf, cleaned up fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,49 +1,60 @@
-if test -f $HOME/.config/fish/config.local.fish
-    source $HOME/.config/fish/config.local.fish
+function install_eza
+    if not type -q eza
+        echo "installing eza."
+        sudo apt install eza
+    end
+    alias la 'eza -a --color=always --group-directories-first --icons'
+    alias ll 'eza -l --color=always --group-directories-first --icons'
+    alias ls 'eza -al --color=always --group-directories-first --icons'
+    alias lt 'eza -l --color=always --group-directories-first --icons -s=modified -r'
 end
+
+function install_zoxide
+    if not type -q zoxide
+        echo "installing zoxide."
+        curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+    end
+    zoxide init fish | source
+end
+
+function install_fzf
+    if not type -q fzf
+        echo "installing fzf."
+        sudo apt install fzf
+    end
+end
+
+function initialize_conda
+    # !! Contents within this block are managed by 'conda init' !!
+    if test -f $HOME/anaconda3/bin/conda
+        status is-interactive && eval /home/gregf/anaconda3/bin/conda "shell.fish" hook $argv | source
+    else
+        if test -f "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+            . "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH $HOME/anaconda3/bin $PATH
+        end
+    end
+end
+
+fish_add_path $HOME/.bin
+fish_add_path $HOME/.local/bin
 
 alias v nvim
 alias ip "ip -c"
 alias ipa "ip -4 -br addr"
 alias lll "ls | grep '>'" # only symbolic links
 
-if not type -q eza
-    echo "installing eza."
-    sudo apt install eza
-end
+set -x EDITOR nvim
 
-alias la 'eza -a --color=always --group-directories-first --icons'
-alias ll 'eza -l --color=always --group-directories-first --icons'
-alias ls 'eza -al --color=always --group-directories-first --icons'
-alias lt 'eza -l --color=always --group-directories-first --icons -s=modified -r'
+install_eza
+install_zoxide
+install_fzf
+initialize_conda
 
-fish_add_path $HOME/.bin
-fish_add_path $HOME/.local/bin
-
-# zoxide
-if not type -q zoxide
-    echo "installing zoxide."
-    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-end
-zoxide init fish | source
-
-if not type -q fzf
-    echo "installing fzf."
-    sudo apt install fzf
-end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f $HOME/anaconda3/bin/conda
-    status is-interactive && eval /home/gregf/anaconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
-        . "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH $HOME/anaconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
-
-# Dotfiles alias
 alias dotfiles='/usr/bin/git --git-dir=/home/gregf/.dotfiles/ --work-tree=/home/gregf'
+
+# source local fish config if exists
+if test -f $HOME/.config/fish/config.local.fish
+    source $HOME/.config/fish/config.local.fish
+end
